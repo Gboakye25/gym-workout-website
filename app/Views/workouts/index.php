976514afe249aa6a11e8090d3da4fa8275457f3b
Godfred Boakye <G.boakye@wlv.ac.uk>
2026-03-13 04:10:14 +0000

    Added workout list view

---
+++ app/Views/workouts/index.php
@@ -0,0 +1,13 @@
+<?= $this->include('layouts/header') ?>
+
+<h2><?= $title ?></h2>
+
+<p>Here are some gym workouts:</p>
+
+<ul>
+    <li>Chest Workout</li>
+    <li>Leg Workout</li>
+    <li>Back Workout</li>
+</ul>
+
+<?= $this->include('layouts/footer') ?>
