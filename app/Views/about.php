4b067701d31b8f3b592de92d8d95fad343c016bf
Godfred Boakye <G.boakye@wlv.ac.uk>
2026-03-13 03:44:23 +0000

    Added home and about views

---
+++ app/Views/about.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>About This Website</h1>
+    <p>This website is being built with CodeIgniter 4 for an Advanced Web Development assignment.</p>
+</body>
+</html>
---
+++ app/Views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome to My Gym Workout Website</h1>
+    <p>This website will help users explore gym workouts and fitness information.</p>
+</body>
+</html>
