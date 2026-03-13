b135560f0cd80b6170944edf465ab0e5f774d204
Godfred Boakye <G.boakye@wlv.ac.uk>
2026-03-13 04:41:54 +0000

    Added workout details controller method

--- app/Controllers/Workout.php
+++ app/Controllers/Workout.php
@@ -9,11 +9,41 @@ class Workout extends BaseController
         $data = [
             'title' => 'Workout List',
             'workouts' => [
-    ['slug' => 'chest-workout', 'name' => 'Chest Workout'],
-    ['slug' => 'leg-workout', 'name' => 'Leg Workout'],
-    ['slug' => 'back-workout', 'name' => 'Back Workout']
-]
+                ['slug' => 'chest-workout', 'name' => 'Chest Workout'],
+                ['slug' => 'leg-workout', 'name' => 'Leg Workout'],
+                ['slug' => 'back-workout', 'name' => 'Back Workout']
+            ]
+        ];
 
         return view('workouts/index', $data);
+    }
+
+    public function view($slug = null)
+    {
+        $workouts = [
+            'chest-workout' => [
+                'name' => 'Chest Workout',
+                'description' => 'This workout focuses on building chest strength.'
+            ],
+            'leg-workout' => [
+                'name' => 'Leg Workout',
+                'description' => 'This workout focuses on lower body strength.'
+            ],
+            'back-workout' => [
+                'name' => 'Back Workout',
+                'description' => 'This workout focuses on back and posture.'
+            ]
+        ];
+
+        if (! array_key_exists($slug, $workouts)) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        $data = [
+            'title' => $workouts[$slug]['name'],
+            'workout' => $workouts[$slug]
+        ];
+
+        return view('workouts/view', $data);
     }
 }
